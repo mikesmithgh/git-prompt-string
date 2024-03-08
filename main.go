1959b6625bf10f78434df52e79319514b7754c23
Mike Smith <10135646+mikesmithgh@users.noreply.github.com>
2024-03-08 14:26:26 -0500

    chore: add cli flags

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"path"
@@ -14,23 +15,45 @@ import (
 	"github.com/pelletier/go-toml/v2"
 )
 
+var (
+	configPath      = flag.String("config", "", "")
+	promptPrefix    = flag.String("prompt-prefix", "  ", "")
+	promptSuffix    = flag.String("prompt-suffix", "", "")
+	aheadFormat     = flag.String("ahead-format", "", "")
+	behindFormat    = flag.String("behind-format", "", "")
+	divergedFormat  = flag.String("diverged-format", "", "")
+	colorEnabled    = flag.Bool("color-enabled", true, "")
+	colorClean      = flag.String("color-clean", "green", "")
+	colorConflict   = flag.String("color-conflict", "yellow", "")
+	colorDirty      = flag.String("color-dirty", "red", "")
+	colorUntracked  = flag.String("color-untracked", "magenta", "")
+	colorNoUpstream = flag.String("color-no-upstream", "bright-black", "")
+)
+
 func main() {
 
 	cfg := config.BgpsConfig{
-		PromptPrefix:    "  ",
-		PromptSuffix:    "",
-		AheadFormat:     "↑[%d]",
-		BehindFormat:    "↓[%d]",
-		DivergedFormat:  "↕ ↑[%d] ↓[%d]",
-		ColorEnabled:    true,
-		ColorClean:      "green",
-		ColorConflict:   "yellow",
-		ColorDirty:      "red",
-		ColorUntracked:  "magenta",
-		ColorNoUpstream: "bright-black",
+		PromptPrefix:    *promptPrefix,
+		PromptSuffix:    *promptSuffix,
+		AheadFormat:     *aheadFormat,
+		BehindFormat:    *behindFormat,
+		DivergedFormat:  *divergedFormat,
+		ColorEnabled:    *colorEnabled,
+		ColorClean:      *colorClean,
+		ColorConflict:   *colorConflict,
+		ColorDirty:      *colorDirty,
+		ColorUntracked:  *colorUntracked,
+		ColorNoUpstream: *colorNoUpstream,
 	}
 
-	bgpsConfigPath := os.Getenv("BGPS_CONFIG")
+	flag.Parse()
+
+	var bgpsConfigPath string
+	if *configPath == "" {
+		bgpsConfigPath = os.Getenv("BGPS_CONFIG")
+	} else {
+		bgpsConfigPath = *configPath
+	}
 	if bgpsConfigPath == "" {
 		xdgConfigHome := os.Getenv("XDG_CONFIG_HOME")
 		if xdgConfigHome == "" {
@@ -58,6 +81,33 @@ func main() {
 			util.ErrMsg("unmarshal config", err, 0)
 		}
 	}
+
+	flag.Visit(func(f *flag.Flag) {
+		switch f.Name {
+		case "prompt-prefix":
+			cfg.PromptPrefix = f.Value.String()
+		case "prompt-suffix":
+			cfg.PromptSuffix = f.Value.String()
+		case "ahead-format":
+			cfg.AheadFormat = f.Value.String()
+		case "behind-format":
+			cfg.BehindFormat = f.Value.String()
+		case "diverged-format":
+			cfg.DivergedFormat = f.Value.String()
+		case "color-enabled":
+			cfg.ColorEnabled = f.Value.String() == f.DefValue
+		case "color-clean":
+			cfg.ColorClean = f.Value.String()
+		case "color-conflict":
+			cfg.ColorConflict = f.Value.String()
+		case "color-dirty":
+			cfg.ColorDirty = f.Value.String()
+		case "color-untracked":
+			cfg.ColorUntracked = f.Value.String()
+		case "color-no-upstream":
+			cfg.ColorUntracked = f.Value.String()
+		}
+	})
 
 	if !cfg.ColorEnabled {
 		color.Disable()
