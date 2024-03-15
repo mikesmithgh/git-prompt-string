b11fed3afb87b9df7aca8b87c335b1215d01e8f1
Mike Smith <10135646+mikesmithgh@users.noreply.github.com>
2024-03-15 08:20:03 -0400

    chore: add rebase test

--- integration/bgps_test.go
+++ integration/bgps_test.go
@@ -31,6 +31,7 @@ func TestBGPS(t *testing.T) {
 		{"rebase_m", []string{"--config=NONE"}, "\x1b[34m \ue0a0 main|REBASE-m 1/1\x1b[0m", nil},
 		{"am_rebase", []string{"--config=NONE"}, "\x1b[34m \ue0a0 (b69e688)|AM/REBASE 1/1\x1b[0m", nil},
 		{"am", []string{"--config=NONE"}, "\x1b[34m \ue0a0 (b69e688)|AM 1/1\x1b[0m", nil},
+		{"rebase", []string{"--config=NONE"}, "\x1b[34m \ue0a0 main|REBASE 1/1\x1b[0m", nil},
 
 		{"clean", []string{"--config=NONE", "--color-enabled=false"}, " \ue0a0 main", nil},
 		{"clean", []string{"--config=NONE", "--color-enabled=false", "--prompt-prefix= start "}, " start main", nil},
