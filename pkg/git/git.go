27480809eddbf7494123a3a8e36c34d1d6ccd709
Mike Smith <10135646+mikesmithgh@users.noreply.github.com>
2024-03-20 22:56:49 -0400

    chore: debug

--- pkg/git/git.go
+++ pkg/git/git.go
@@ -250,7 +250,7 @@ func HasCleanWorkingTree() (bool, error) {
 }
 
 func BranchRemote(branch string) (string, error) {
-	fmt.Printf(" ** branch is %s ** ", branch)
+	fmt.Printf(" branch is %s ", branch)
 	// cmd := exec.Command(
 	// 	"git",
 	// 	"config",
